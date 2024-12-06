e99ddccb52bbfded2b952ba1c86805c3c61c51f6
fly_mc <me@flymc.cc>
2024-12-06 20:34:52 +0800

    Revert "feat: sort emoji categories"
    This reverts commit 8006dd8004b21e24b8f9e5758fe119c698af73a8.

--- packages/frontend/src/custom-emojis.ts
+++ packages/frontend/src/custom-emojis.ts
@@ -17,7 +17,7 @@ export const customEmojiCategories = computed<[ ...string[], null ]>(() => {
 			categories.add(emoji.category);
 		}
 	}
-	return markRaw([...(Array.from(categories).sort()), null]);
+	return markRaw([...Array.from(categories), null]);
 });
 
 export const customEmojisMap = new Map<string, Misskey.entities.EmojiSimple>();
