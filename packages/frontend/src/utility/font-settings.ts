72e91e72da41d27a27de795be4f4156208468a36
FLY_MC <me@flymc.cc>
2025-05-29 04:46:11 +0800

    fix font settings

--- packages/frontend/src/utility/font-settings.ts
+++ packages/frontend/src/utility/font-settings.ts
@@ -55,14 +55,6 @@ function getFontId(name: string, option: string) {
 	}
 }
 
-async function loadFontStyle(fontId: string) {
-	try {
-		await import(`@/styles-font/${fontId}.scss`);
-	} catch (e) {
-		console.warn(`Failed to load font style: ${fontId}`, e);
-	}
-}
-
 export function getDefaultFontSettings() {
 	const def_arr = miLocalStorage.getItem('defaultFontFace')?.split('_');
 	const fontFace = ref(def_arr?.[0] ?? 'maokentangyuan');
